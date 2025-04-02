77edd5ae9ee1fecf1b967de481d8c9beae25f82b
Jatin Nanda <571629+xebu@users.noreply.github.com>
2025-04-02 18:45:54 +0100

    update health probe info

--- server.js
+++ server.js
@@ -13,16 +13,9 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello, World! Is there anyone out there?');
 });
 
-// const health = express();
-// const HEALTH_PORT = 9999;
-
 app.get('/health', (_req, res) => {
   res.status(200).send('OK');
 });
 app.get('/healthz', (_req, res) => {
   res.status(200).send('healthz::: is OK 2');
 });
-
-// health.listen(HEALTH_PORT, '0.0.0.0', () => {
-//   console.log(`Health check running at http://localhost:${HEALTH_PORT}/healthz`);
-// });
