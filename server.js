1a7faa10ea5d38bd2ca5fba8b3a317bffa86cacb
Jatin Nanda <571629+xebu@users.noreply.github.com>
2025-04-03 15:00:01 +0100

    change port from 3000 to 9999

--- server.js
+++ server.js
@@ -1,40 +1,27 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9999;
 
 app.use(express.static('public'));
 
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
+
+  if ('healthz' in req.query) {
+    return res.status(200).send('✅ Probe OK: /?healthz');
+  }
+
   res.status(200).send('Hello, World! Is there anyone out there???');
 });
 
-app.get('/health', (_req, res) => {
+app.get('/health', (req, res) => {
   res.status(200).send('OK - health');
 });
 
-app.get('/healthz', (_req, res) => {
-  res.status(200).send('OK - healthz 🎶');
+app.get('/healthz', (req, res) => {
+  res.status(200).send('healthz probe OK direct');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Main app running at http://localhost:${PORT}`);
-});
-
-// 👉 Separate listener on port 9999 for readiness probe
-const health = express();
-
-health.get('/', (req, res) => {
-  if ('healthz' in req.query) {
-    return res.status(200).send('✅ Probe OK: /?healthz');
-  }
-  res.status(200).send('Not found - in route ');
-});
-
-health.get('/healthz', (_req, res) => {
-  res.status(200).send('healthz probe OK direct');
-});
-
-health.listen(9999, '0.0.0.0', () => {
-  console.log(`🔍 Health check probe running at http://localhost:9999/healthz`);
 });
