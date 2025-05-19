598f86edfa81f751a1d787591f7b5e86d691d3b3
Dmitri Algazin <Dmitri.Algazin1@justice.com>
2025-05-19 15:17:04 +0100

    feat: each controller wrapped in to asyncMiddleware for better error handling;

--- server/routes/case.ts
+++ server/routes/case.ts
@@ -6,38 +6,63 @@ import { courtInformationController } from '../controllers/court-information-con
 import courtInfoHealthCheck from '../controllers/courtInfoController'
 import { courtInformationTwoController } from '../controllers/court-information-two-controller'
 import { AuthenticatedUser } from '../helpers/user-status'
+import asyncMiddleware from '../middleware/asyncMiddleware'
 
 export default function routes(app: express.Express): void {
   // Page: Select your case
-  app.get('/case/select', AuthenticatedUser, async (req, res, next) => {
-    caseSelectController(req, res, next)
-  })
+  app.get(
+    '/case/select',
+    AuthenticatedUser,
+    asyncMiddleware((req, res, next) => {
+      caseSelectController(req, res, next)
+    }),
+  )
 
   // Page: Case dashboard
-  app.get('/case/dashboard', AuthenticatedUser, async (req, res, next) => {
-    caseDashboardController(req, res, next)
-  })
+  app.get(
+    '/case/dashboard',
+    AuthenticatedUser,
+    asyncMiddleware((req, res, next) => {
+      caseDashboardController(req, res, next)
+    }),
+  )
 
   // TODO: add `:id` to route - View court information
   // INFO: This route is still to be used for prototype purposes
-  app.get('/case/court-information', AuthenticatedUser, async (req, res, next) => {
-    courtInformationController(req, res, next, 'pages/case/court-information')
-  })
+  app.get(
+    '/case/court-information',
+    AuthenticatedUser,
+    asyncMiddleware((req, res, next) => {
+      courtInformationController(req, res, next, 'pages/case/court-information')
+    }),
+  )
 
   // INFO: This route has been added for show & tell 29-Apr-2025
   // It breaks GDS principles and requires further discussion
-  app.get('/case/court-information-2', AuthenticatedUser, async (req, res, next) => {
-    courtInformationTwoController(req, res, next, 'pages/case/court-information-2')
-  })
+  app.get(
+    '/case/court-information-2',
+    AuthenticatedUser,
+    asyncMiddleware((req, res, next) => {
+      courtInformationTwoController(req, res, next, 'pages/case/court-information-2')
+    }),
+  )
 
   // TODO: add `:id` to route - View contact details
   // TODO: need to verify if contact details should be linked to a case
   // TODO: need to determine if the contact details should be in a different module
-  app.get('/case/contact-details', AuthenticatedUser, async (req, res, next) => {
-    courtInformationController(req, res, next, 'pages/case/contact-details')
-  })
+  app.get(
+    '/case/contact-details',
+    AuthenticatedUser,
+    asyncMiddleware((req, res, next) => {
+      courtInformationController(req, res, next, 'pages/case/contact-details')
+    }),
+  )
 
-  app.get('/case/court-info-health', AuthenticatedUser, async (req, res, next) => {
-    courtInfoHealthCheck(req, res, next)
-  })
+  app.get(
+    '/case/court-info-health',
+    AuthenticatedUser,
+    asyncMiddleware((req, res, next) => {
+      courtInfoHealthCheck(req, res, next)
+    }),
+  )
 }
--- server/routes/oneLogin.ts
+++ server/routes/oneLogin.ts
@@ -5,28 +5,40 @@ import { authorizeController } from '../controllers/authorize/authorize-controll
 import { callbackController } from '../controllers/callback/callback-controller'
 import { logoutController } from '../controllers/logout/logout-controller'
 import { signedInController } from '../controllers/signed-in-controller'
+import asyncMiddleware from '../middleware/asyncMiddleware'
 
 export default function routes(app: express.Express): void {
   const clientConfig = OneLoginConfig.getInstance()
 
-  app.get('/oidc/login', (req: Request, res: Response, next: NextFunction) =>
-    authorizeController(req, res, next, false),
+  app.get(
+    '/oidc/login',
+    asyncMiddleware((req: Request, res: Response, next: NextFunction) => authorizeController(req, res, next, false)),
   )
 
-  app.get('/oidc/verify', (req: Request, res: Response, next: NextFunction) =>
-    authorizeController(req, res, next, true),
+  app.get(
+    '/oidc/verify',
+    asyncMiddleware((req: Request, res: Response, next: NextFunction) => authorizeController(req, res, next, true)),
   )
 
-  app.get('/oidc/authorization-code/callback', (req: Request, res: Response, next: NextFunction) =>
-    callbackController(req, res, next),
+  app.get(
+    '/oidc/authorization-code/callback',
+    asyncMiddleware((req: Request, res: Response, next: NextFunction) => callbackController(req, res, next)),
   )
 
-  app.get('/signed-in', AuthenticatedUser, (req: Request, res: Response, next: NextFunction) => {
-    signedInController(req, res, next)
-  })
+  app.get(
+    '/signed-in',
+    AuthenticatedUser,
+    asyncMiddleware((req: Request, res: Response, next: NextFunction) => {
+      signedInController(req, res, next)
+    }),
+  )
 
-  // Page: Select your case
-  app.get('/oidc/logout', (req: Request, res: Response, next: NextFunction) => logoutController(req, res, next))
+  app.get(
+    '/oidc/logout',
+    asyncMiddleware((req: Request, res: Response, next: NextFunction) => {
+      logoutController(req, res, next)
+    }),
+  )
 
   // app.get("/landing-page", (req: Request, res: Response, next: NextFunction) => {
   //   // set flag to say user came via post office landing page
@@ -36,9 +48,12 @@ export default function routes(app: express.Express): void {
   //   authorizeController(req, res, next, false)
   // });
 
-  app.get('/signed-out', (req: Request, res: Response) => {
-    res.render('pages/signed-out.njk', {
-      serviceName: 'Track My Case',
-    })
-  })
+  app.get(
+    '/signed-out',
+    asyncMiddleware((req: Request, res: Response, next: NextFunction) => {
+      res.render('pages/signed-out.njk', {
+        serviceName: 'Track My Case',
+      })
+    }),
+  )
 }
