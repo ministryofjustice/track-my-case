d42dad9f67e81751a4b3e756f19eb2bf0ff217d2
Dmitri Algazin <dmitri.algazin@hmcts.net>
2025-07-23 10:24:55 +0100

    Feat/vp 33 search case by urn (#118)
    * VP-33: [UI] Enable Victim to Enter Case URN and Submit Request Description;
    VP-34: [UI] Display Case Tracking Results upon 'Track My Case' Button Click;
    * chore: version and content updates;
    
    ---------
    
    Co-authored-by: Dmitri Algazin <Dmitri.Algazin1@justice.com>

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { logger } from './server/logger'
   app.listen(app.get('port'), () => {
     const port = `${app.get('port')}`
     logger.info(`🚀 Server started on port ${port}`)
-    logger.info(`  🔗 Track my case:  http://localhost:${port}`)
+    logger.info(`  🔗 Track My Case:  http://localhost:${port}`)
     logger.info(`  🏥 Health check:   http://localhost:${port}/health`)
     logger.info(`  🧪 Test service: http://localhost:${port}/case/court-info-health`)
 
